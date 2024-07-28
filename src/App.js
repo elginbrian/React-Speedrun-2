650e52e334a61d9b0d398363cda8c02cb24b591c
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-07-28 16:29:25 +0700

    feat: firebase config

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from "react";
 import { Navbar } from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./page/Home";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <div className="App text-white">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<h1>Sign In</h1>} />
-        <Route path="/login" element={<h1>Login</h1>} />
-      </Routes>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<h1>Sign In</h1>} />
+          <Route path="/login" element={<h1>Login</h1>} />
+        </Routes>
+      </AuthContextProvider>
     </div>
   );
 }
